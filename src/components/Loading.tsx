9ab9e3d54ff58dd5ac2c1e82db5c5023b9d55428
Lauri Pesonen <lauri.pesonen@iki.fi>
2026-02-11 17:44:42 +0000

    refactor: migrate Loading.tsx from Chakra Spinner to Tailwind

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,8 @@
-import { Spinner } from "@chakra-ui/react";
-
 function Loading() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-      }}
-    >
-      <Spinner size="xl" />
-      <div>Loading flight data...</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-blue-500" />
+      <div className="mt-2">Loading flight data...</div>
     </div>
   );
 }
